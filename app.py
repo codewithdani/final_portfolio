eca9249eafbcf448311ae0fc9cad6bb7b14db5a0
Daniel <daniel@gmail.com>
2024-02-13 21:44:05 +0300

    updated

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify, session, redirect, url_for
 import requests
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import *
 from datetime import datetime
 import sqlite3
 
